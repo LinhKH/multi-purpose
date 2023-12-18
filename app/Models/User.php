1afcef11236bd7f91e86f34f7d132a5a26cb95cf
Kieu Hoa LInh <l_kieu@vision-net.co.jp>
2023-12-18 16:47:26 +0700

    part 13.2: format date using accessor

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    protected $appends = [
+        'formated_created_at'
+    ];
+
+    public function getFormatedCreatedAtAttribute() {
+        return $this->created_at->format(config('app.date_format'));
+    }
 }
