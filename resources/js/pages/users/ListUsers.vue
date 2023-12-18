72f178ac613d1e7e007cfce1f2d6b1f0fafabb73
Kieu Hoa LInh <l_kieu@vision-net.co.jp>
2023-12-18 18:03:16 +0700

    part 14: Use php8.1 Enums

--- resources/js/pages/users/ListUsers.vue
+++ resources/js/pages/users/ListUsers.vue
@@ -177,7 +177,7 @@ onMounted(() => {
 									<td>{{ user.name }}</td>
 									<td>{{ user.email }}</td>
 									<td>{{ formatDate(user.created_at) }}</td>
-									<td>John Doe</td>
+									<td>{{ user.role }}</td>
 									<td>
                                         <a href="#" @click.prevent="editUser(user)"><i class="fa fa-edit"></i></a>
                                         <a href="#" @click.prevent="confirmUserDeletion(user.id)"><i class="fa fa-trash text-danger ml-2"></i></a>
